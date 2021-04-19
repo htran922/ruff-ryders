09d41353a7ff26efe16807b5797f3945dcaf5d4f
ALM <ashleymanning@Ashleys-MBP.fios-router.home>
2021-04-19 15:45:45 -0400

    added adoptablepets seeder

--- server/db/Seeder.js
+++ server/db/Seeder.js
@@ -4,12 +4,37 @@ import LineReader from "line-reader"
 import { fileURLToPath } from "url"
 
 const pool = new pg.Pool({
-  connectionString: "postgres://postgres:password@localhost:5432/ruff_ryders_db" })
+  connectionString: "postgres://postgres:password@localhost:5432/ruff_ryders_db"
+})
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const adoptablePetsPath = path.join(__dirname, "../../adoptablepets.txt")
 class Seeder {
-  static async seed() {
-    // your seeder code here
+  static async seedAdoptablePets() {
+ 
+    LineReader.eachLine(adoptablePetsPath, async (line, last, done) => {
+      const [name, img_url, age, vaccination_status, adoption_story, available_for_adoption, pet_type_id] = line.split(";")
+      
+      const queryString = "INSERT INTO adoptable_pets  (name, img_url, age, vaccination_status, adoption_story, available_for_adoption, pet_type_id ) VALUES ($1, $2, $3, $4, $5, $6, $7);"
+
+      try {
+        const result = await pool.query(queryString, [name, img_url, age, vaccination_status, adoption_story, available_for_adoption, pet_type_id])
+        if (last) {
+     
+          console.log("Seeding Complete")
+          pool.end()
+        }
+        done()
+      } catch (error) {
+        console.log(`Error: ${error}`)
+        done()
+      }
+    })
   }
+
 }
+
 
 export default Seeder
---
+++ server/db/schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS adoptable_pets CASCADE;
+
+CREATE TABLE adoptable_pets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  age INTEGER , 
+  vaccination_status BOOLEAN NOT NULL,
+  adoption_story TEXT NOT NULL
+  available_for_adoption BOOLEAN NOT NULL,
+  pet_type_id REFERENCES pet_types(id),
+);
+
+ALTER TABLE adoptable_pets
+ALTER COLUMN vaccination_status
+SET DEFAULT FALSE;
+
+ALTER TABLE adoptable_pets
+ALTER COLUMN available_for_adoption
+SET DEFAULT TRUE;
