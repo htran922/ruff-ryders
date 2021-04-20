92220bec0d0e36bd4dd1716c6b54f2fe121b6e6f
Helen <helentran.dev@gmail.com>
2021-04-20 18:11:08 -0400

    added links for specific pet type page

--- client/components/PetTypeTile.js
+++ client/components/PetTypeTile.js
@@ -1,14 +1,18 @@
 import React from "react"
+import { Link } from "react-router-dom"
+import _ from "lodash"
 
 const PetTypeTile = props => {
   let imgURL = props.url
 
   return (
-    <li>
-      <p>{props.type}</p>
-      <img src={imgURL}></img>
+    <div>
+      <Link to={`/pets/${props.type}`}>{_.capitalize(props.type)}</Link>
+      <Link to={`/pets/${props.type}`}>
+        <img src={imgURL} />
+      </Link>
       <p>{props.description}</p>
-    </li>
+    </div>
   )
 }
 
