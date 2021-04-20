f817081f7d915c7c0db145afcc1e9a4a6268a853
Helen <helentran.dev@gmail.com>
2021-04-20 15:57:02 -0400

    add import statement to rootRouter

--- server/routes/rootRouter.js
+++ server/routes/rootRouter.js
@@ -1,11 +1,12 @@
 import express from "express"
 import clientRouter from "./clientRouter.js"
+import petTypeRouter from "./api/v1/petTypeRouter.js"
 import adoptablePetsRouter from "./api/v1/adoptablePetsRouter.js"
 
 const rootRouter = new express.Router()
 
-rootRouter.use("/api/v1/adoptable-pets", adoptablePetsRouter)
 rootRouter.use("/api/v1/pet-types", petTypeRouter)
+rootRouter.use("/api/v1/adoptable-pets", adoptablePetsRouter)
 
 rootRouter.use("/", clientRouter)
 
