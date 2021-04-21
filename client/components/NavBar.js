7e62e83e6b795d741b2017bc29c811950af23f19
unknown <katame1321@gmail.com>
2021-04-21 11:52:04 -0700

    changed all quote to be the same and removed unused code

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Route, Switch, BrowserRouter, Redirect, Link } from "react-router-dom"
+import { Route, Switch, Redirect, Link } from 'react-router-dom'
 
-import PetTypesList from "./PetTypesList.js"
-import AdoptablePetType from "./AdoptablePetType"
+import PetTypesList from './PetTypesList.js'
+import AdoptablePetType from './AdoptablePetType'
 import AdoptablePetTypeShow from './AdoptablePetTypeShow'
 import SurrenderForm from './SurrenderForm'
 
@@ -14,14 +14,14 @@ const NavBar = props =>{
       <Link to='/pets/cats'>Cats</Link>
       <Link to='/pets/dogs'>Dogs</Link>
       <Link to='/pets/foxes'>Foxes</Link>
-      <Link to="/adoption/new">Surrender Your Pet</Link>
+      <Link to='/adoption/new'>Surrender Your Pet</Link>
       
       <Switch>
-        <Route exact path="/">
-          <Redirect to="/pets" />
+        <Route exact path='/'>
+          <Redirect to='/pets' />
         </Route>
-        <Route exact path="/pets" component={PetTypesList} />
-        <Route exact path = "/pets/:type" component={AdoptablePetType} />
+        <Route exact path='/pets' component={PetTypesList} />
+        <Route exact path = '/pets/:type' component={AdoptablePetType} />
         <Route exact path ='/pets/:type/:id' component = {AdoptablePetTypeShow} />
         <Route exact path = '/adoptions/new' component = {SurrenderForm} />
       </Switch>
