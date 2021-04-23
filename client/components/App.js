ed27f75df83b09ee552df1491e312d4284438f24
Helen <helentran.dev@gmail.com>
2021-04-22 22:05:49 -0400

    fix adoptions/new path and fix navbar link

--- client/components/App.js
+++ client/components/App.js
@@ -5,14 +5,7 @@ import "foundation-sites"
 import $ from "jquery"
 import "../assets/scss/main.scss"
 
-
 import NavBar from './NavBar.js'
-
-import { Route, BrowserRouter} from "react-router-dom"
-import PetTypesList from "./PetTypesList.js"
-import AdoptablePetType from "./AdoptablePetType.js"
-import SurrenderForm from "./SurrenderForm.js"
-import AdoptablePetTypeShow from "./AdoptablePetTypeShow.js"
 
 const App = props => {
   useEffect(() => {
@@ -22,15 +15,6 @@ const App = props => {
   return (
     <BrowserRouter>
       <Route path='/' component={NavBar} />
-      <Switch>
-        <Route exact path="/">
-          <Redirect to="/pets" />
-        </Route>
-        <Route exact path="/pets" component={PetTypesList} />
-        <Route exact path="/pets/:type" component={AdoptablePetType} />
-        <Route exact path="/surrenders/new" component={SurrenderForm} />
-        <Route exact path="/pets/:type/:id" component={AdoptablePetTypeShow} />
-      </Switch>
     </BrowserRouter>
   )
 }
--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,33 +1,40 @@
-import React from 'react'
-import { Route, Switch, Redirect, Link } from 'react-router-dom'
+import React from "react"
+import { Route, Switch, Redirect, Link } from "react-router-dom"
 
-import PetTypesList from './PetTypesList.js'
-import AdoptablePetType from './AdoptablePetType'
-import AdoptablePetTypeShow from './AdoptablePetTypeShow'
-import SurrenderForm from './SurrenderForm'
+import PetTypesList from "./PetTypesList.js"
+import AdoptablePetType from "./AdoptablePetType"
+import AdoptablePetTypeShow from "./AdoptablePetTypeShow"
+import SurrenderForm from "./SurrenderForm"
 
-const NavBar = props =>{
-  return(
-    <div>
+const NavBar = props => {
+  return (
+    <div className="navbar-container">
+      <div className="menu-item">
+        <Link to="/pets">Welcome Page</Link>
+      </div>
+      <div className="menu-item">
+        <Link to="/pets/cat">Cats</Link>
+      </div>
+      <div className="menu-item">
+        <Link to="/pets/dog">Dogs</Link>
+      </div>
+      <div className="menu-item">
+        <Link to="/pets/fox">Foxes</Link>
+      </div>
+      <div className="menu-item">
+        <Link to="/adoptions/new">Surrender Your Pet</Link>
+      </div>
 
-      <Link to='/pets'>Welcome Page</Link>
-      <Link to='/pets/cats'>Cats</Link>
-      <Link to='/pets/dogs'>Dogs</Link>
-      <Link to='/pets/foxes'>Foxes</Link>
-      <Link to='/adoption/new'>Surrender Your Pet</Link>
-      
       <Switch>
-        <Route exact path='/'>
-          <Redirect to='/pets' />
+        <Route exact path="/">
+          <Redirect to="/pets" />
         </Route>
-        <Route exact path='/pets' component={PetTypesList} />
-        <Route exact path = '/pets/:type' component={AdoptablePetType} />
-        <Route exact path ='/pets/:type/:id' component = {AdoptablePetTypeShow} />
-        <Route exact path = '/adoptions/new' component = {SurrenderForm} />
+        <Route exact path="/pets" component={PetTypesList} />
+        <Route exact path="/pets/:type" component={AdoptablePetType} />
+        <Route exact path="/pets/:type/:id" component={AdoptablePetTypeShow} />
+        <Route exact path="/adoptions/new" component={SurrenderForm} />
       </Switch>
     </div>
-
-    
   )
 }
 
