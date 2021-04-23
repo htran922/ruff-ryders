3384486614144f906c0f6a702a1ec9db316c9ccc
unknown <katame1321@gmail.com>
2021-04-22 18:00:06 -0700

    removed un-necessary file

--- server/db/addApplicationData.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO surrender_applications (adoptable_pet_id, name, phone_number, email, status)
-
-VALUES
-(1, 'Bob Farkel','1234567890','bobfarkel@gmail.com','accepted'),
-(2, 'Dude Manguy', '2545678901','dudemanguy@gmail.com', 'accepted'),
-(3, 'Jean Falcoroy', '2532545547', 'ihateanimal@gmail.com', 'accepted'),
-(4, 'Herpy DaDerpy', '2066666667', 'humptydumpty@gmail.com','accepted'),
-(5, 'Louis Kent', '2068675309','iloveclarkkent@hotmail.com', 'accepted');
