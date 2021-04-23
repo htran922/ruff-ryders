ebbefdfd252d2aecf68283f2c560dfed2d77c62f
unknown <katame1321@gmail.com>
2021-04-22 17:57:56 -0700

    added seeder for surrender_applications

--- server/db/Seeder.js
+++ server/db/Seeder.js
@@ -12,12 +12,14 @@ const __dirname = path.dirname(__filename)
 
 const petTypesPath = path.join(__dirname, "../../petTypes.json")
 const adoptablePetsPath = path.join(__dirname, "../../adoptablePets.json")
+const surrenderApplicationsPath = path.join(__dirname, "../../surrenderApplications.json")
 
 class Seeder {
   static async seed() {
     try {
       const petTypesData = JSON.parse(fs.readFileSync(petTypesPath)).petTypes
       const adoptablePetsData = JSON.parse(fs.readFileSync(adoptablePetsPath)).adoptablePets
+      const surrenderApplicationsData = JSON.parse(fs.readFileSync(surrenderApplicationsPath)).surrenderApplication
 
       for (let i = 0; i < petTypesData.length; i++) {
         const petType = petTypesData[i]
@@ -34,6 +36,7 @@ class Seeder {
       const foxData = await pool.query("SELECT * FROM pet_types WHERE type = 'fox';")
       const fox = foxData.rows[0]
 
+      const petIds = []
       for (let i = 0; i < adoptablePetsData.length; i++) {
         const adoptablePet = adoptablePetsData[i]
         switch (adoptablePet.type) {
@@ -57,6 +60,7 @@ class Seeder {
           adoption_story,
           available_for_adoption
         } = adoptablePet
+        petIds[name] = i + 1
         const queryString =
           "INSERT INTO adoptable_pets (name, img_url, age, vaccination_status, adoption_story, available_for_adoption, pet_type_id) VALUES ($1, $2, $3, $4, $5, $6, $7);"
         await pool.query(queryString, [
@@ -68,6 +72,28 @@ class Seeder {
           available_for_adoption,
           adoptablePet.type.id
         ])
+      }
+
+      for(let i = 0; i < surrenderApplicationsData.length; i++){
+        const surrenderApplication = surrenderApplicationsData[i]
+        const {
+          pet_name,
+          name,
+          phone_number,
+          email,
+          status
+        } = surrenderApplication
+        const thisPetId = petIds[pet_name]
+        
+        const appQueryString = 
+          "INSERT INTO surrender_applications (adoptable_pet_id, name, phone_number, email, status) VALUES ($1, $2, $3, $4, $5);"
+          await pool.query(appQueryString, [
+            thisPetId,
+            name,
+            phone_number,
+            email,
+            status
+          ])
       }
       console.log("Seeding complete")
       pool.end()
